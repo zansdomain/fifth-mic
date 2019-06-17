372318f23c479553a90d56cdc56c3f9e0d99c4a6
zansdomain <51897426+zansdomain@users.noreply.github.com>
2019-06-17 18:28:38 -0500

    Ugh

--- studio/schemas/objects/magic.js
+++ studio/schemas/objects/magic.js
@@ -63,7 +63,7 @@ export default {
                    title: "Stylo",
                    name: "stylo",
                    type: "string",
-                   }
+                   },
              }
           ]
          }
