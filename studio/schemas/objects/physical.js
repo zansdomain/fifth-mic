5049c822ba18efa89885fe55b954bb2c7b9ac41a
zansdomain <51897426+zansdomain@users.noreply.github.com>
2019-06-17 00:33:54 -0500

    Add files via upload

---
+++ studio/schemas/objects/physical.js
@@ -0,0 +1,39 @@
+// The schema for Character physical object type
+{
+  title: 'Physical',
+  name: 'physical',
+  type: 'object',
+  fields: [
+    {
+      title: 'Physicality',
+      name: 'physicality',
+      type: 'number'
+    },
+    {
+      title: 'Age',
+      name: 'age',
+      type: 'number'
+    },
+    {
+      title: 'Gender',
+      name: 'gender',
+      type: 'array'
+      of: [{type: "string"}
+    },
+   {
+     title: 'Government tag',
+  name: 'gtag',
+  type: 'string',
+    },
+    {
+      title: 'Origin',
+      name: 'origin',
+      type: 'string'
+    },
+    {
+      title: 'Height',
+      name: 'height',
+      type: 'number'
+    },
+  ]
+},
