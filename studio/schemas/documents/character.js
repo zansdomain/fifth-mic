f70b44ffb84ae37fbee4d3eb142aa4d97a90fcc6
zansdomain <51897426+zansdomain@users.noreply.github.com>
2019-06-17 02:04:25 -0500

    Add files via upload

---
+++ studio/schemas/documents/character.js
@@ -0,0 +1,31 @@
+// character.js
+
+export default {
+  type: 'document',
+  name: 'character',
+  title: 'Character',
+  fields: [
+    {
+      name: 'avatar',
+      title: 'Avatar',
+      type: 'image'
+    },
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string'
+    },
+    {
+    name: 'physical'
+    title: 'Stats'
+    type: 'reference'	
+    to: [{type: 'physical'}]
+    },
+    {
+      name: 'magic',
+      title: 'Magic',
+      type: 'reference'
+      to: [{type: 'magical'}]
+    },
+    ]
+}
