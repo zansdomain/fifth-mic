4d7a19ee041f69d8c6b4fee829cf369160af4e89
zansdomain <51897426+zansdomain@users.noreply.github.com>
2019-06-25 21:01:48 -0500

    Character up the grid

--- web/components/SessionGrid.vue
+++ web/components/SessionGrid.vue
@@ -1,69 +1,34 @@
 <template>
-  <ul v-if="filteredSessions" class="sessionGrid">
-    <li v-for="session in filteredSessions" :key="session._id" class="session">
-      <nuxt-link :to="`/sessions/${session._id}`">
+  <ul class="sessionGrid">
+    <li v-for="character in Characters" :key="character._id" class="session">
+      <nuxt-link :to="`/characters/${character._id}`">
         <span class="top">
           <span>
-            <span class="type">{{ session.sessionType }}</span>
-            <h2 class="sessionTitle">{{ session.title }}</h2>
-          </span>
-          <span
-            class="time"
-            :title="session.fromTime | date('MMMM DD YYYY h:MM a')"
-          >
-            {{ session.fromTime | date('h:MM a') }}
+            <span class="type">{{ character.characterType }}</span>
+            <h2 class="sessionTitle">{{ character.name }}</h2>
           </span>
         </span>
         <p>
-          {{ session.summary }}
+          Later put magic here
         </p>
       </nuxt-link>
-      <PersonsList
-        v-if="showPersons && session.persons"
-        :persons="session.persons"
-      />
     </li>
   </ul>
 </template>
 
 <script>
-import PersonsList from '~/components/PersonsList'
-import { dateFilter } from 'vue-date-fns'
+import clist from '~/components/clist'
 
 export default {
-  filters: {
-    date: dateFilter
-  },
+ 
   components: {
-    PersonsList
+    clist
   },
   props: {
-    showPersons: {
+    showCharacters: {
       type: Boolean,
       default: false
     },
-    sessions: {
-      type: Array,
-      default: () => []
-    }
-  },
-  computed: {
-    filteredSessions: props => {
-      return props.sessions.map(session => {
-        return {
-          ...session,
-          ...session.session,
-          persons:
-            session.persons &&
-            session.persons.filter(
-              person =>
-                person.person &&
-                person.person.slug &&
-                person.person.slug.current
-            )
-        }
-      })
-    }
   }
 }
 </script>
